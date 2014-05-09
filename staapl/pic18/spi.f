821204fc5a8b00fd0173759907368c283b8e6d8e
Tom Schouten <tom@zwizwa.be>
2014-05-09 17:47:06 -0400

    spi.f

---
+++ staapl/pic18/spi.f
@@ -0,0 +1,48 @@
+\ SPI
+
+\ Serial Data Out (SDO) – RC7/RX/DT/SDO
+\ Serial Data In (SDI)  – RB0/AN12/INT0/FLT0/SDI/SDA
+\ Serial Clock (SCK)    – RB1/AN10/INT1/SCK/SCL
+\ Slave Select (SS)     – RA5/AN4/SS/HLVDIN/C2OUT
+
+\ CPL : clock polarity, clock idle state: 0/1
+\ CKE : 1 tx at active -> idle
+\       0 tx at idle -> active
+
+
+macro
+: config-spi-master | CPL CKE SMP |
+    TRISC 7 low  \ SDO
+    TRISB 1 low  \ SCK
+    TRISA 5 high \ SS
+
+    CPL 7 <<<
+    CKE 6 <<< or SSPSTAT !
+
+    CPL 4 <<< #x22 or SSPCON1 !
+    ;
+
+: init-spi-master 0 0 0 config-spi-master ;
+
+: test-spi init-spi-master 0 begin dup spi-tx/rx drop 1+ again ;
+   
+\ : init-spi-slave \ --
+\     TRISC 7 low  \ SDI
+\     TRISB 1 high \ SCK
+\     TRISA 5 high \ SS
+
+\     #x24 SSPCON1 !  \ master, FOSC/16, CPL=0
+
+\     \ before enabling SPI mode, the clock line must be in the idle state.
+    
+\     ;
+
+: spi-wait do SSPSTAT 0 high? until ;
+: spi-tx/rx \ out - in
+    SSPBUF !
+    spi-wait
+    SSPBUF @ ;
+
+    
+    
+forth
