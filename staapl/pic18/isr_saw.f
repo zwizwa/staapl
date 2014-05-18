69f8d16273aac33cc1d80824a9ec0c4f4b4dfd0e
Tom Schouten <tom@zwizwa.be>
2014-05-17 22:58:47 -0400

    notes

--- staapl/pic18/isr_saw.f
+++ staapl/pic18/isr_saw.f
@@ -9,9 +9,14 @@ forth
 
 : hi-isr
     8 org-begin
+\    0 ,,
+\    0 ,,
+\    0 ,,
+\    0 ,,
     discharge high
     PIR2 CMIF low   \ ack interrupt
     discharge low
     1 retfie        \ fast return
     org-end
+
 
