0d317d60641a732d733b6d99f7a369fbff3c609a
Tom Schouten <tom@zwizwa.be>
2014-07-04 11:33:32 -0400

    tweak default stack sizes

--- staapl/pic18/init-stacks.f
+++ staapl/pic18/init-stacks.f
@@ -3,11 +3,17 @@ macro
 : ds! 1 - 0 lfsr ;
 : xs! 1 - 1 lfsr ;
 
-\ Should work for all 18F devices    
+\ Should work for all 18F devices
+\ Stacks grow from low to high addresses
+
+\ Note that in "clean" Forth code, the data stack will never grow very
+\ large.  However the retain stack might get large depending on depth
+\ of context (e.g. shallow / dynamic binding of variables).
+
 : init-stacks-simple
     0 rs!       \ hardware return stack
-    #x80 ds!    \ data stack
-    #xC0 xs!    \ aux stack
+    #x80 ds!    \ data stack 80-8F 32 bytes
+    #xA0 xs!    \ aux stack  90-FF 96 bytes
 ;    
     
 forth
