1a6c9b2f01f4e2e27342f2b5db3411070a893caf
Tom Schouten <tom@zwizwa.be>
2014-07-03 21:51:29 -0400

    4-byte midi writes

--- staapl/pic18/test-midi.f
+++ staapl/pic18/test-midi.f
@@ -1,23 +1,23 @@
 
 \ Midi connected to EP 3
-: midi-begin 3 IN-begin ;
+: midi-begin 3 4 IN-begin ;
 : midi-end   IN-end 3 IN-flush ;   
 
     
 : note-on \ note --
     midi-begin
-    #x09 >a  \ cable, class
-    #x90 >a  \ note on channel 0
-         >a  \ note value
-    127  >a  \ velocity
+        #x09 >a  \ cable, class
+        #x90 >a  \ note on channel 0
+             >a  \ note value
+        127  >a  \ velocity
     midi-end ;
     
 : note-off \ note --
     midi-begin
-    #x08 >a  \ cable, class
-    #x80 >a  \ note on channel 0
-         >a  \ note value
-    127  >a  \ velocity
+        #x08 >a  \ cable, class
+        #x80 >a  \ note on channel 0
+             >a  \ note value
+        127  >a  \ velocity
     midi-end ;
     
     
