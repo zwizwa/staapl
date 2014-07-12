80700fbdc5c3340c8986b357a4ac5dbc7df71200
Tom Schouten <tom@zwizwa.be>
2014-07-11 23:12:48 -0400

    bcr2000

--- app/bcr2000.sh
+++ app/bcr2000.sh
@@ -1,4 +1,6 @@
 #aconnect BCR2000:0 'USB Device 0x5f9:0xfff0 MIDI 1':0   # knobs
 #aconnect BCR2000:1 'USB Device 0x5f9:0xfff0 MIDI 1':0   # midi in
-aconnect BCR2000:0 32:0   # knobs
-aconnect BCR2000:1 32:0   # midi in
+
+DEV=20
+aconnect BCR2000:0 $DEV:0   # knobs
+aconnect BCR2000:1 $DEV:0   # midi in
