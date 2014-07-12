40e8d74ecf0730a8cf1f4b412df93f411271ac98
Tom Schouten <tom@zwizwa.be>
2014-07-12 15:26:35 -0400

    bcr2000.sh

--- app/bcr2000.sh
+++ app/bcr2000.sh
@@ -1,6 +1,7 @@
 #aconnect BCR2000:0 'USB Device 0x5f9:0xfff0 MIDI 1':0   # knobs
 #aconnect BCR2000:1 'USB Device 0x5f9:0xfff0 MIDI 1':0   # midi in
 
-DEV=20
-aconnect BCR2000:0 $DEV:0   # knobs
-aconnect BCR2000:1 $DEV:0   # midi in
+DEV=`aconnect -o|grep 0x5f9:0xfff0|grep client|awk '{print $2}'`0
+
+aconnect BCR2000:0 $DEV   # knobs
+aconnect BCR2000:1 $DEV   # midi in
