1a40ee688316d84614a7b919e0e637d18974156e
Tom Schouten <tom@zwizwa.be>
2015-01-02 21:05:51 -0500

    cleanup

--- sm/actors.rkt
+++ sm/actors.rkt
@@ -2,7 +2,17 @@
 (require data/queue
          racket/match)
 
-(define any '_)
+
+;; Actor evalutation.
+
+;; Note sure where this is going next.  The idea is to distill state
+;; machine DAGs from the collection of events that can be passed into
+;; an actor network.  My guess is that with proper representation,
+;; some of the control flow (if + function call) should be possible to
+;; reconstruct.
+
+;; However, it might be necessary to open up the representation of
+;; lambda expressions.
 
 (define-struct process (mailbox handlers) #:mutable #:transparent)
 
@@ -35,7 +45,7 @@
          ((list-rest tag body)
           (let ((fn
                  (or (lookup tag handlers)
-                     (lookup any handlers)
+                     (lookup '_  handlers)
                      (error `(message-not-handled ,tag ,body)))))
             (apply fn body))))))))
 
@@ -67,34 +77,39 @@
     (lambda () process)))
 
 
+;; Allow sequential code to continue after a receive block.
 
-;; ------------- TEST --------------
+;; Captures continuation in a thunk so it can be shared across
+;; branches of the message dispatch
 
-;; Allow sequential code to continue after a receive block.  The low
-;; level representation uses a closure to represent the resume point
-;; explicitly.
+;; Note that this implementation has leaky continuations due to
+;; improper tail calls being used.  This is OK for finite runs, which
+;; is the whole idea.
 
-;; Implementation leaks thunks due to improper tail calls being used.
-;; This only works for finite runs!
-
-(define-syntax seq
+(define-syntax actor-begin
   (syntax-rules (receive)
     ((_) (void))
     ((_ (receive ((tag . args) body ...) ...) . rest)
-     (let ((rest-thunk (fun () . rest)))
+     (let ((rest-thunk (actor-lambda () . rest)))
        (receive/suspend
-        `((tag . ,(fun args
-                       body ...
-                       (rest-thunk)))
+        `((tag . ,(actor-lambda args
+                    body ...
+                    (rest-thunk)))
           ...))))
     ((_ statement . rest)
      (begin
        statement
-       (seq . rest)))))
+       (actor-begin . rest)))))
 
-(define-syntax-rule (fun args . body)
+(define-syntax-rule (actor-lambda args . body)
   (lambda args
-    (seq . body)))
+    (actor-begin . body)))
+
+
+
+
+
+;; ------------- TEST --------------
 
 
 (define (test1)
@@ -126,44 +141,38 @@
 
 
 
-
+;; same but sugared
 (define (test2)
   (letrec
       ((alice
         (spawn
-         (fun ()
-              (printf "alice boot\n")
-              (send (bob) '(hi from-alice))
-              (receive
-                  ((hi . args)
-                   (printf "got hi ~a\n" args)))
-              (receive
-                  ((_)
-                   (error 'alice-not-reached))))
+         (actor-lambda ()
+           (printf "alice boot\n")
+           (send (bob) '(hi from-alice))
+           (receive
+               ((hi . args)
+                (printf "alice got hi ~a\n" args)))
+           (receive
+               ((_)
+                (error 'alice-not-reached))))
          '()))
        (bob
         (spawn
-         (fun ()
-              (printf "bob boot\n")
-              (receive
-                  ((hi . args)
-                   (printf "got hi ~a\n" args)
-                   (send (alice) '(hi from-bob))))
-              (receive
-                  ((_)
-                   (error 'bob-not-reached))))
+         (actor-lambda ()
+           (printf "bob boot\n")
+           (receive
+               ((hi . args)
+                (printf "bob got hi ~a\n" args)
+                (send (alice) '(hi from-bob))))
+           (receive
+               ((_)
+                (error 'bob-not-reached))))
          '())))
     (run)))
   
 
 
 
-;(test2)
-
-;; (define test3
-;;   (fun ()
-;;        (receive ((_)))
-;;        (receive ((_)))
-;;        ))
+(test2)
        
        
