4c27dc57059b01c99510006ef54f9f260f7e9249
Tom Schouten <tom@zwizwa.be>
2016-08-18 19:25:58 -0400

    console.c : numbers and commands (todo: parameterize addresses)

--- app/console.c
+++ app/console.c
@@ -18,11 +18,23 @@ struct word word[] = {
     {"relay!",1267},
     {}
 };
+
 FILE *console_file;
+uint8_t get(void) {
+    int c;
+    if (EOF == (c=fgetc(console_file))) return -1;
+    // printf("%02x ", c);  // debug only
+    return c;
+}
 void transaction(void *buf, int nb_bytes) {
     fwrite(buf,1,nb_bytes,console_file);
     fflush(console_file);
-    // READ
+
+    get();
+    uint8_t n = get();
+    for (int i = 0; i<n; i++) { get(); }
+    printf("OK\n");
+    fflush(stdout);
 }
 
 int main(int argc, const char **argv) {
@@ -47,7 +59,7 @@ int main(int argc, const char **argv) {
                             addr = word[i].addr;
                         }
                     }
-                    printf("word: %s %d\n", buf, addr);
+                    //printf("word: %s %d\n", buf, addr);
                     if (addr) {
                         uint8_t msg[] = {0, 3, 3, addr<<1, addr>>7};
                         transaction(msg,sizeof(msg));
@@ -58,7 +70,7 @@ int main(int argc, const char **argv) {
                 }
                 else {
                     uint8_t byte = atoi(buf);
-                    printf("number: %d\n", byte);
+                    //printf("number: %d\n", byte);
                     uint8_t msg[] = {0, 3, 1, 1, byte};
                     transaction(msg,sizeof(msg));
                 }
