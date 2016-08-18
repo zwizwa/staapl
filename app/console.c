98e3088dc1cc584059dc51f4f756ef3b2fd3e9c6
Tom Schouten <tom@zwizwa.be>
2016-08-18 19:16:36 -0400

    console.c

--- app/console.c
+++ app/console.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdint.h>
 
 /* Minimalistic console to interface textual commands to Staapl monitor. */
 
@@ -14,17 +15,22 @@ struct word {
 };
 
 struct word word[] = {
-    {"relay!",1234},
+    {"relay!",1267},
     {}
 };
+FILE *console_file;
+void transaction(void *buf, int nb_bytes) {
+    fwrite(buf,1,nb_bytes,console_file);
+    fflush(console_file);
+    // READ
+}
 
 int main(int argc, const char **argv) {
     const char *console = "/dev/ttyACM0";
     if (argc >= 2) {
         console = argv[1];
     }
-    FILE *f = fopen(console, "w+");
-    if (!f) return -1;
+    if (!(console_file = fopen(console, "w+"))) return -1;
 
     unsigned int size = 0;
     unsigned int nondigits = 0;
@@ -42,9 +48,19 @@ int main(int argc, const char **argv) {
                         }
                     }
                     printf("word: %s %d\n", buf, addr);
+                    if (addr) {
+                        uint8_t msg[] = {0, 3, 3, addr<<1, addr>>7};
+                        transaction(msg,sizeof(msg));
+                    }
+                    else {
+                        exit(-1);
+                    }
                 }
                 else {
-                    printf("number: %d\n", atoi(buf));
+                    uint8_t byte = atoi(buf);
+                    printf("number: %d\n", byte);
+                    uint8_t msg[] = {0, 3, 1, 1, byte};
+                    transaction(msg,sizeof(msg));
                 }
             }
             size = 0;
