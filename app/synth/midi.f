5ac8bdd4f6481801c928613eae711905ecb5bead
Tom Schouten <tom@zwizwa.be>
2017-05-30 22:41:28 +0200

    fix program change parsing + store in patch variable

--- app/synth/midi.f
+++ app/synth/midi.f
@@ -20,9 +20,10 @@ variable midi-byte2 : m2 midi-byte2 @ ;
 \ During silence we need to save synth config.
 variable synth-save    
 
-\ Main pitch
-2variable period
- 
+
+\ Current MIDI state
+2variable period \ Period converted from midi note
+variable patch   \ Current MIDI program / synth patch
 
 : m-interpret \ --
     m0 midi-cmd route
@@ -38,7 +39,7 @@ variable synth-save
 : 9x ~chan  m1 m2 0 = if notes-remove else notes-add then play-last ;
 : 8x ~chan  m1 notes-remove play-last ;
 : Bx ~chan  continuous-controller  ;
-: Cx ~chan  m1 program-change ;    
+: Cx ~chan  m1 patch ! ;
 : Ex ~chan  m1 m2 pitch-mod 2! ;
     
 
@@ -80,14 +81,6 @@ variable synth-save
         ____ . ____ . ____ . ____ . ____ . ____ . ____ . ____ . ____ . ____ . ____ . ____ . ____ . ____ . ____ . ____ ; \ 7
 
 
-\ Eventually it might be interesting to find out a generic patch
-\ format, but for now these are best pre-defined.
-        
-: program-change \ program --
-    2 min route
-        square .
-        ;
-    
 
 \ USB PACKET / STREAM
 
@@ -114,6 +107,14 @@ variable synth-save
 \ well-formed.
 
 
+\ MIDI data payload size depends on message type:
+\ 8 2 note-off
+\ 9 2 note-on
+\ A 2 key-pressure
+\ B 2 control-change
+\ C 1 program-change        
+\ D 1 channel-pressure
+\ E 2 pitch-bend
         
 \ NOT TESTED       
 : midi>m \ --
@@ -122,13 +123,13 @@ variable synth-save
     dup midi-byte0 !
     midi-cmd route
         midi>m12 . midi>m12 . midi>m12 . midi>m12 . \ 8 9 A B
-        midi>m12 . midi>m1  . midi>m12 .          ; \ C D E F
+        midi>m1  . midi>m1  . midi>m12 .          ; \ C D E F
 
 : midi-continuation \ byte1 --
     midi-byte1 !
     m0 midi-cmd route
         midi>m2 . midi>m2 . midi>m2 . midi>m2 . \ 8 9 A B
-        midi>m2 .         . midi>m2 .         ; \ C D E F
+                .         . midi>m2 .         ; \ C D E F
         
 : midi>m1  midi> midi-byte1 ! ;
 : midi>m2  midi> midi-byte2 ! ;
